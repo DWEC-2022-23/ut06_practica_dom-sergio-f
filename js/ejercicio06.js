af228986752598fc4d017640eb01b5acdaf947b6
sergiofepinilla <sergiofepinilla@gmail.com>
2023-01-27 09:59:47 +0200

    Update Ejercicio 6

--- js/ejercicio06.js
+++ js/ejercicio06.js
@@ -49,6 +49,25 @@ let addItemButton = "";
 
  function crearbotones(event) {
   // Realiza las acciones de los botones subir, borrar y bajar para los elementos de la lista
+    if (event.target.className == "subir") {
+      let li = event.target.parentNode;
+      let prevLi = li.previousElementSibling;
+      let ul = li.parentNode;
+      if (prevLi) {
+        ul.insertBefore(li, prevLi);
+      }
+    } else if (event.target.className == "bajar") {
+      let li = event.target.parentNode;
+      let nextLi = li.nextElementSibling;
+      let ul = li.parentNode;
+      if (nextLi) {
+        ul.insertBefore(nextLi, li);
+      }
+    } else if (event.target.className == "borrar") {
+      let li = event.target.parentNode;
+      let ul = li.parentNode;
+      ul.removeChild(li);
+    }
  }
 
  function MostrarOcultarLista(){
@@ -59,14 +78,22 @@ let addItemButton = "";
     listDiv.classList.toggle('hidden');
   }
  }
+
  function CambiarTextoLista(){
   //Modifica  el texto de la lista (descriptionP) con el valor del input (descriptionInput).
   // Inicialmente COSAS QUE SON VIOLETA
   descriptionP.innerText = descriptionInput.value;
  }
+ 
  function AñadirElemento(){
   //Añade un nuevo elemento a la lista con el valor del input (addItemInput). 
   //Recuerda que el elemento tendrá que tener sus botones de subir, bajar y borrar.
+  let inputValue = addItemInput.value;
+  let li = document.createElement("li");
+  li.innerText = inputValue;
+  listUl.appendChild(li);
+  attachListItemButtons(li);
+  addItemInput.value = "";
  }
 
 
